41b15ba56c8cbae565062150c424c85e858c5cee
Baha2000 <dragon.baha@yandex.ru>
2021-07-05 16:08:29 +0300

    init commit

---
+++ Listing/MyDoubleList.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listing
+{
+    class MyDoubleList
+    {
+        int Count;
+        int Capacity;
+        double[] Mas;
+        double[] TempMas;
+        public double this[int index]
+        {
+            get
+            {
+                return Mas[index];
+            }
+        }
+        public MyDoubleList(int Capacity)
+        {
+            this.Capacity = Capacity;
+            Mas = new double[Capacity];
+            Count = 0;
+        }
+
+        public MyDoubleList()
+        {
+            Capacity = 1;
+            Mas = new double[1];
+            Count = 0;
+        }
+
+        public void Add(double value)
+        {
+            CheckCount();
+            InsertItem(value);
+        }
+
+        public void AddRange(IEnumerable<double> items)
+        {
+            CheckCount(items.Count());
+
+            foreach (double value in items)
+            {
+                InsertItem(value);
+            }
+
+        }
+
+        public void Remove(int index)
+        {
+            if (index <= Count)
+            {
+                for (int i = index; i < Count - 1; i++)
+                {
+                    Mas[i] = Mas[i + 1];
+                }
+                Mas[Count - 1] = 0;
+                Count--;
+            }
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < Count; i++)
+                Console.WriteLine(Mas[i]);
+        }
+
+        private void CheckCount(int length)
+        {
+
+            if (Count + length > Capacity)
+            {
+                TempMas = new double[Capacity];
+                Mas.CopyTo(TempMas, 0);
+                Mas = new double[Count + length];
+                TempMas.CopyTo(Mas, 0);
+                Capacity += length;
+            }
+        }
+
+        private void CheckCount()
+        {
+
+            if (Count + 1 > Capacity)
+            {
+                TempMas = new double[Capacity];
+                Mas.CopyTo(TempMas, 0);
+                Mas = new double[Count + 1];
+                TempMas.CopyTo(Mas, 0);
+                Capacity++;
+            }
+        }
+
+        private void InsertItem(double value)
+        {
+            Mas[Count] = value;
+            Count++;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return Mas.GetEnumerator();
+        }
+    }
+}
---
+++ Listing/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Listing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myList = new MyDoubleList();
+            double[] array = new double[] { 222.0, 260.5, 289.6, 291.2 };
+            double val;
+
+            myList.Add(10.0);
+            myList.Add(5.0);
+            myList.Add(3.0);
+            myList.Add(13.5);
+            myList.Add(18.9);
+            myList.Add(23.7);
+            myList.Add(16.2);
+            myList.AddRange(array);
+            val = myList[3];
+            myList.Remove(0);
+            myList.Print();
+            
+            foreach (var value in myList)
+            {
+                Console.Write(value + " ");
+            }
+            Console.WriteLine(val);
+            Console.ReadKey();
+        }
+    }
+}
