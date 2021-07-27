f5d51edde9ee7197ec6eef85d2783a21f9018e5e
Baha2000 <dragon.baha@yandex.ru>
2021-07-27 14:01:10 +0300

    Добавлен дженерик класс и дополнительный класс Enumerator для закольцовывания

---
+++ Listing/MyDoubleListG.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listing
+{
+    class MyDoubleListG<T>
+    {
+        public int Count { get; private set; }
+        public int Capacity { get; private set; }
+
+        private T[] myArray;
+        public T this[int index]
+        {
+            get
+            {
+                if (index >= 0)
+                    index = CheckIndex(index);
+                else
+                    throw new Exception("Недопустимый индекс");
+                return myArray[index];
+            }
+        }
+
+        public MyDoubleListG(int Capacity) //Конструктор, для ввода пользователем вместимости массива вручную
+        {
+            this.Capacity = Capacity;
+            myArray = new T[Capacity];
+            Count = 0;
+        }
+
+        public MyDoubleListG() //Дефолтный конструктор
+        {
+            Capacity = 1;
+            myArray = new T[1];
+            Count = 0;
+        }
+
+        public void Add(T value)
+        {
+            CheckArray(1);
+            InsertItem(value);
+        }
+
+        public void AddRange(IEnumerable<T> items)
+        {
+            CheckArray(items.Count());
+            foreach (T value in items)
+            {
+                InsertItem(value);
+            }
+        }
+
+        public void Remove(int index)
+        {
+            if (index <= Count - 1)
+            {
+                for (int i = index; i < Count - 1; i++)
+                {
+                    myArray[i] = myArray[i + 1];
+                }
+                myArray[Count - 1] = default(T);
+                Count--;
+            }
+            else
+                throw new Exception("Индекс больше, чем количество элементов в массиве");
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                Console.WriteLine(myArray[i]);
+                if (i + 1 == Count)
+                    i = 0;
+            }
+        }
+
+        private void CheckArray(int length)
+        {
+                myArray = MasCopy(ref myArray, CheckCapacity(length));
+        }
+
+        private T[] MasCopy(ref T[] sourceArray, int length) // Передается ссылка на массив myArray для его дальнейшего пересоздания и вставки в него значений из return
+        {
+            T[] destinationArray = new T[length];
+
+            if (length > Capacity)
+            {
+                System.Array.Copy(sourceArray, destinationArray, sourceArray.Length); //Если нужна копия для увеличения массива
+                Capacity = length;
+                sourceArray = new T[length];
+            }
+            else
+                System.Array.Copy(sourceArray, destinationArray, length); //Если нужен чистый массив значений
+            return destinationArray;
+        }
+
+        private void InsertItem(T value)
+        {
+            myArray[Count] = value;
+            Count++;
+        }
+
+        private int CheckIndex(int index) //рекурсия для получения индекса в закольцованном буфере
+        {
+            if (index > Count - 1)
+                index = CheckIndex(index - Count);
+            return index;
+        }
+
+        private int CheckCapacity(int length) //проверка на достаточную вместимость для вставки в лист
+        {
+            if (Count + length > Capacity)
+            {
+                if (length == 1)
+                    return Capacity * Capacity;
+                else
+                    return Capacity + length * length;
+            }
+            return length;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return new MyEnumerator<T>(MasCopy(ref myArray, Count));
+        }
+
+    }
+}
---
+++ Listing/MyEnumerator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listing
+{
+    class MyEnumerator<T> : IEnumerator
+    {
+        private int index;
+        T[] myArray;
+
+        public MyEnumerator(T[] myArray)
+        {
+            index = 0;
+            this.myArray = myArray;
+        }
+        public object Current
+        {
+            get
+            {
+                if (!CheckIndex(index))
+                    throw new Exception("Недопустимый индекс");
+                return myArray[index];
+            }
+        }
+
+        public bool MoveNext()
+        {
+            if (!CheckIndex(++index))
+                Reset();
+            return true;
+        }
+
+        public void Reset()
+        {
+            index = 0;
+        }
+
+        private bool CheckIndex(int index)
+        {
+            if (index > myArray.Length - 1 || index < 0)
+                return false;
+            return true;
+        }
+    }
+}
